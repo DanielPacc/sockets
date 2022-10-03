cf17a17be120a1f664e64e70d1e31febe7561818
lai2-01 <lai2-01@itismeucci.com>
2022-10-03 10:25:35 +0200

    servers con

---
+++ src/main/java/it/fi/meucci/App.java
@@ -0,0 +1,15 @@
+package it.fi.meucci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        server serverA=new server();
+        serverA.attendi();
+        serverA.comunica();
+    }
+}
---
+++ src/main/java/it/fi/meucci/server.java
@@ -0,0 +1,44 @@
+package it.fi.meucci;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+public class server {
+
+    ServerSocket server=null;
+    Socket client=null;
+    String stringaRicevuta=null;
+    String stringamodificata=null;
+    BufferedReader indalClient;
+    DataOutputStream outversoClient;
+
+    public Socket attendi(){
+
+        try{
+            System.out.println("1, server partito");
+            server = new ServerSocket(6789);
+            client=server.accept();
+            server.close();
+            indalClient= new BufferedReader(new InputStreamReader(client.getInputStream()));
+            outversoClient= new DataOutputStream(client.getOutputStream());
+        }
+        catch(Exception e){}
+        return client;
+    }
+
+    public void comunica(){
+        try{
+        System.out.println("3, benvenuto, scrivi dai");
+        stringaRicevuta = indalClient.readLine();
+        System.out.println("6, ricevuta "+stringaRicevuta);
+        stringamodificata=stringaRicevuta.toUpperCase();
+        System.out.println("7, invio la stringa modificata");
+        outversoClient.writeBytes(stringamodificata+'\n');
+        System.out.println("vaffammpc daje un po");
+        client.close();
+
+    }catch(Exception e){}
+}
+    
+}
